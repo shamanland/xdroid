c442a2a0488f1c31f5bdd1e08772cca97d1538b3
Oleksii Kropachov <o.kropachov@shamanland.com>
2014-07-04 22:55:23 +0300

    Fixed unit-tests.

--- lib-core/src/test/java/xdroid/core/ObjectsTest.java
+++ lib-core/src/test/java/xdroid/core/ObjectsTest.java
@@ -1,17 +1,23 @@
-package xdroid.core.test;
-
-import xdroid.core.Objects;
+package xdroid.core;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import static org.junit.Assert.assertSame;
+
 @Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
 public class ObjectsTest {
     @Test(expected = NullPointerException.class)
-    public void notNull() throws Exception {
+    public void notNullWithNull() throws Exception {
         Objects.notNull(null);
+    }
+
+    @Test
+    public void notNullWithObject() throws Exception {
+        Object obj = new Object();
+        assertSame(obj, Objects.notNull(obj));
     }
 }
