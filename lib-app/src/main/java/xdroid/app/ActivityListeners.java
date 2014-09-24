da0d1872b8fc27fda3540cde81876fb91960010c
Oleksii Kropachov <o.kropachov@shamanland.com>
2014-09-24 20:40:05 +0300

    Added possibility to add custom listener for OnBackPressed() and finish() methods.

---
+++ lib-app/src/main/java/xdroid/app/ActivityListeners.java
@@ -0,0 +1,59 @@
+package xdroid.app;
+
+import java.util.Collection;
+
+import static java.util.Collections.emptyList;
+import static xdroid.collections.Prototypes.newArrayList;
+import static xdroid.core.Objects.notNull;
+
+/**
+ * @author Oleksii Kropachov (o.kropachov@shamanland.com)
+ */
+class ActivityListeners {
+    private Collection<OnBackPressedListener> mBackPressed;
+    private Collection<OnFinishListener> mFinish;
+
+    public Iterable<OnBackPressedListener> getBackPressed() {
+        if (mBackPressed != null) {
+            return mBackPressed;
+        }
+
+        return emptyList();
+    }
+
+    public Iterable<OnFinishListener> getFinish() {
+        if (mFinish != null) {
+            return mFinish;
+        }
+
+        return emptyList();
+    }
+
+    public void add(OnBackPressedListener listener) {
+        if (mBackPressed == null) {
+            mBackPressed = newArrayList();
+        }
+
+        mBackPressed.add(notNull(listener));
+    }
+
+    public void add(OnFinishListener listener) {
+        if (mFinish == null) {
+            mFinish = newArrayList();
+        }
+
+        mFinish.add(notNull(listener));
+    }
+
+    public void remove(OnBackPressedListener listener) {
+        if (mBackPressed != null) {
+            mBackPressed.remove(listener);
+        }
+    }
+
+    public void remove(OnFinishListener listener) {
+        if (mFinish != null) {
+            mFinish.remove(listener);
+        }
+    }
+}
---
+++ lib-app/src/main/java/xdroid/app/OnFinishListener.java
@@ -0,0 +1,8 @@
+package xdroid.app;
+
+/**
+ * @author Oleksii Kropachov (o.kropachov@shamanland.com)
+ */
+public interface OnFinishListener {
+    void onFinish(ActivityExt activity);
+}
